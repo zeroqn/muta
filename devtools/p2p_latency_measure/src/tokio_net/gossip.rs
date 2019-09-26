9b00ce5d5a281c48da1c39955c6cd4df941829c3
zero.qn <zero.qn@outlook.com>
2019-09-26 18:48:12 +0800

    fix(p2p_latency_measure): stream closed

--- devtools/p2p_latency_measure/src/tokio_net/gossip.rs
+++ devtools/p2p_latency_measure/src/tokio_net/gossip.rs
@@ -1,5 +1,6 @@
-use std::{io::Error, net::SocketAddr};
+use std::{io::Error, net::SocketAddr, sync::Arc, thread, time::Duration};
 
+use parking_lot::RwLock;
 use async_trait::async_trait;
 use derive_more::Constructor;
 use futures::{
@@ -20,25 +21,30 @@ use tokio::{
     net::tcp::TcpStream,
 };
 
+type GossipStreams = Arc<RwLock<Vec<UnboundedSender<Result<Bytes, Error>>>>>;
+
 #[derive(Clone, Constructor)]
 pub struct TokioGossip {
-    streams: Vec<UnboundedSender<Result<Bytes, Error>>>,
+    streams: GossipStreams,
 }
 
 impl TokioGossip {
     pub fn from(socket_addrs: Vec<SocketAddr>) -> Self {
-        let mut streams = Vec::new();
+        let streams = Arc::new(RwLock::new(Vec::new()));
 
         for addr in socket_addrs.into_iter() {
             let (tx, rx) = unbounded();
             let stream_fut = TcpStream::connect(&addr);
-
-            streams.push(tx);
+            let streams = Arc::clone(&streams);
 
             let gossip_msg = async move {
+                thread::sleep(Duration::from_secs(3));
+
                 let mut rx = rx.boxed().compat();
                 let stream = stream_fut.compat().await.expect("tcp stream");
                 let gossip_framed = Framed::new(stream, LengthDelimitedCodec::new());
+
+                streams.write().push(tx);
 
                 if gossip_framed.send_all(&mut rx).compat().await.is_err() {
                     info!("gossip stream closed");
@@ -59,8 +65,9 @@ impl Gossip for TokioGossip {
         M: MessageCodec,
     {
         let bytes = msg.encode().await?;
+        let streams = self.streams.read();
 
-        for stream in self.streams.iter() {
+        for stream in streams.iter() {
             stream
                 .unbounded_send(Ok(bytes.clone()))
                 .expect("tokio send bytes");
