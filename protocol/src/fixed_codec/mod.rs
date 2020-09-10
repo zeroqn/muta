8c524fc6ee0062410129a11211a259a1cdfd217a
zero.qn <zero.qn@outlook.com>
2020-09-10 20:23:30 +0800

    perf(framework): speed up store map

--- protocol/src/fixed_codec/mod.rs
+++ protocol/src/fixed_codec/mod.rs
@@ -18,11 +18,30 @@ pub trait FixedCodec: Sized {
     fn decode_fixed(bytes: Bytes) -> ProtocolResult<Self>;
 }
 
+impl FixedCodec for [u8; 32] {
+    fn encode_fixed(&self) -> ProtocolResult<Bytes> {
+        Ok(Bytes::copy_from_slice(self))
+    }
+
+    fn decode_fixed(bytes: Bytes) -> ProtocolResult<Self> {
+        if bytes.len() != 32 {
+            return Err(FixedCodecError::NoFixed32Bytes.into());
+        }
+
+        let mut buf = [0u8; 32];
+        buf.copy_from_slice(&bytes[..32]);
+        Ok(buf)
+    }
+}
+
 #[derive(Debug, Display, From)]
 pub enum FixedCodecError {
     Decoder(rlp::DecoderError),
 
     StringUTF8(std::string::FromUtf8Error),
+
+    #[display(fmt = "wrong bytes of [u8; 32]")]
+    NoFixed32Bytes,
 
     #[display(fmt = "wrong bytes of bool")]
     DecodeBool,
