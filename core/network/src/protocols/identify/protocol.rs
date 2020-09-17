c6a569b8c45027cb77b73a1bef080924c8c6cf74
zero.qn <zero.qn@outlook.com>
2020-09-18 01:04:51 +0800

    feat(network): more connecting timeout check

--- core/network/src/protocols/identify/protocol.rs
+++ core/network/src/protocols/identify/protocol.rs
@@ -323,7 +323,10 @@ impl IdentifyProtocol {
 
         match protocol_context.0.session.ty {
             SessionType::Inbound => {
-                log::info!("enter identify inbound procedure for {}", protocol_context.0.session.address);
+                log::info!(
+                    "enter identify inbound procedure for {}",
+                    protocol_context.0.session.address
+                );
 
                 state_context.set_timeout("wait client identity", DEFAULT_TIMEOUT);
 
@@ -333,7 +336,10 @@ impl IdentifyProtocol {
                 };
             }
             SessionType::Outbound => {
-                log::info!("enter identify outbound procedure for {}", protocol_context.0.session.address);
+                log::info!(
+                    "enter identify outbound procedure for {}",
+                    protocol_context.0.session.address
+                );
 
                 self.behaviour.send_identity(&state_context);
                 state_context.set_timeout("wait server ack", DEFAULT_TIMEOUT);
